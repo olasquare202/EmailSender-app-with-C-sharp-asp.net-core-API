bd42aeea6715ef2142a6980d163eb73fd1221ac5
chrisshola1 <tkwalks1@gmail.com>
2022-12-08 15:12:12 +0100

    Add project files.

---
+++ services/EmailServices.cs
@@ -0,0 +1,49 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using SendmailAPI.Interfaces;
+using SendmailAPI.Models;
+using System.Security.Cryptography;
+
+namespace SendmailAPI.services
+{
+    public class EmailServices : IMailServices
+    {
+        private readonly Appsetting _appsetting;
+
+        public EmailServices(IServiceProvider provider)
+        {
+            _appsetting = provider.GetRequiredService<Appsetting>();
+        }
+        public void SendEmail(EmailReceiver emailReceiver)
+        {
+            
+            
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_appsetting.EmailUserName));
+            email.To.Add(MailboxAddress.Parse(emailReceiver.To));
+            email.Subject = emailReceiver.Subject;
+            email.Body = new TextPart(TextFormat.Plain) { Text = emailReceiver.Body};
+            using var smtp = new SmtpClient();
+            smtp.Connect(_appsetting.EmailHost, _appsetting.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_appsetting.EmailUserName, _appsetting.EmailPassword);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+
+            
+
+        }
+        //public static string OTP()
+        //{
+        //    var random = new Random();
+        //    string OTP = string.Empty;
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        OTP = String.Concat(OTP, random.Next(5).ToString());
+        //    }    
+        //    return OTP;
+        //}
+    }
+}
